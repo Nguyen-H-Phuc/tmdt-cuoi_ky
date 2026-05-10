a34b5073683ec1f47200cab59a8d6682ae8a9d18
meomeo2124 <22130064@st.hcmuaf.edu.vn>
2026-05-10 22:35:13 +0700

    feat(frontend): Cập nhật App routing và Header giao diện

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,26 @@ import RegisterPage from "./pages/RegisterPage.jsx";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import CategoryGrid from "./components/CategoryGrid.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 
 function App() {
   return (
       <Router>
         <Routes>
+          {/* Nhóm các trang có chung Header và Footer */}
           <Route element={
             <div className="flex flex-col min-h-screen">
               <Header />
-              <main className="flex-grow">
+              <main className="flex-grow bg-[#F4F4F4]"> {/* Thêm màu nền xám nhạt cho toàn bộ các trang */}
                 <Outlet />
               </main>
               <Footer />
             </div>
           }>
             <Route path="/" element={<CategoryGrid />} />
+
+            {/* 2. Thêm Route Dashboard vào đây */}
+            <Route path="/dashboard" element={<DashboardPage />} />
 
           </Route>
 
