a0005b6296510231d521160bccc20a4c4d0ca6fa
Nguyen Hoang Phuc <181923698+Nguyen-H-Phuc@users.noreply.github.com>
2026-05-22 14:01:51 +0700

    feat(backend): define Order and OrderDetail database models and repositories

---
+++ backend/src/main/java/com/nhom5/backend/repository/OrderDetailRepository.java
@@ -0,0 +1,12 @@
+package com.nhom5.backend.repository;
+
+import com.nhom5.backend.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findByOrder_OrderId(Long orderId);
+}
